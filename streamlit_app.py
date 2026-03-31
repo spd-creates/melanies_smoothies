b77c848ae1322c4d79aa7d5b94a69a29eb284bfb
spd-creates <saumyen91@gmail.com>
2026-03-31 20:26:20 +0530

    Updadted code to match search-on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,14 @@ if ingredients_list:   #if any ingredients exist
     
     for choosen_fruit in ingredients_list:
         ingredients_string += choosen_fruit + ' '
+
+        #gives correct Search-on value
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(choosen_fruit + 'Nutrition Information')
-        # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + choosen_fruit)
+        # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
         
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 

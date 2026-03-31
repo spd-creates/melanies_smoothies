7149bed3e506ad68de14fd5ea439cb1f6c32b7b1
spd-creates <saumyen91@gmail.com>
2026-03-31 19:37:24 +0530

    Added request api calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,11 @@ if ingredients_list:   #if any ingredients exist
 
     #submit button
     time_to_insert = st.button('Submit Order')
+
+#new section to display smoothiefront information using request library
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
         
     
     if time_to_insert:

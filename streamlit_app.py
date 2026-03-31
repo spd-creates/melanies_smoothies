1a98d2c07f326d30991388f141e93c8b6c0b7654
spd-creates <saumyen91@gmail.com>
2026-03-31 20:22:30 +0530

    Add pandas code

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,14 @@ st.write("The name of Smoothie will be: ", name_on_order)
 
 # my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+#convert snowpark dataframe to a Pandas Dataframe
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 
 ingredients_list = st.multiselect (
     'Choose upto 5 ingredients:',
